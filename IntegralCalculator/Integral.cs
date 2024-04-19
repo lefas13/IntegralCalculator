9446c4b8bf49851b18c0cbcf3dea9d5071303225
User <User@WIN-J9S0JJK5DN4>
2024-04-19 23:01:17 +0300

    Написан отчёт к лабораторной работе 5

--- IntegralCalculator/Integral.cs
+++ IntegralCalculator/Integral.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IntegralCalculator
+﻿namespace IntegralCalculator
 {
     // класс Integral с методами для вычисления интеграла
     public class Integral
